c6b0713f4589aafda51b14e47266d81c6cde8c3b
evariste123 <bapfekureraevariste@gmail.com>
2025-11-17 09:37:47 -0800

    project

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+ module.exports = {
+  content: [
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'custom-blue': '#243c5a',
+      },
+    },
+  },
+  plugins: [],
+}
